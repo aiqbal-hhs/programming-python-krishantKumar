e9685a508dfbdf7bba4c160619696efa9b17f5be
krishantKumar <80366809+krishantKumar@users.noreply.github.com>
2021-05-10 10:56:43 +1200

    Add files via upload

---
+++ format-function-may10.py
@@ -0,0 +1,8 @@
+name = input("What is your name?")
+age = input("How old are you?")
+dateofbirth = input("When were you born?")
+occupation = input("What is you occupation?")
+education = input("Where were you educated?")
+networth = input("What is your net worth?")
+favouritefood = input("What is your favourite food?")
+print("I also like {}!".format(favouritefood)
