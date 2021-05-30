13f11e14be05d7568454d5c2936f754cf30623cf
krishantKumar <80366809+krishantKumar@users.noreply.github.com>
2021-05-31 10:33:16 +1200

    Add files via upload

---
+++ comments task python.py
@@ -0,0 +1,12 @@
+#Ask the user for personal details
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where are you from?")
+#Print summary of input for checking 
+print("This is the data you have entered: ")
+
+
+print("Name: " + name)
+print("Age: " + age)
+print("Location: " + location)
+
